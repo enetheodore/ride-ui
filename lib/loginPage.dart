dbe8eb5f8730f7f48e78429899695bcf8849f880
enetheodore <https://github.com/enetheodore/ride-ui>
2024-01-05 11:32:29 +0300

    edited the alignment and width

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -5,32 +5,42 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Login',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            SizedBox(height: 8),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Add your login logic here
-              },
-              child: Text('Login'),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: 350,
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Username'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your login logic here
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/signupPage.dart
+++ lib/signupPage.dart
@@ -12,151 +12,175 @@ class _SignUpPageState extends State<SignUpPage> {
 
   String fullName = '';
   String email = '';
-  String phoneNumber = '';
+  int phoneNumber = 0;
   String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Sign Up')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
             children: [
-              Text(
-                'Sign Up',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Full Name'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your full name';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      fullName = value!;
-                    },
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      email = value!;
-                    },
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                    initialValue:
-                        '', // Set the initial value to the desired phone number
-                    decoration: InputDecoration(labelText: 'Phone Number'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your phone number';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      phoneNumber = value!;
-                    },
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      password = value!;
-                    },
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => VerificationCodePage()),
-                    );
-                  }
-                },
-                child: Text('Sign Up'),
-              ),
-              SizedBox(height: 16),
-              Text('or sign up with'),
-              SizedBox(height: 8),
-              Column(
-                children: [
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Handle sign-up with Gmail
-                    },
-                    icon: Image.asset('assets/image/gmail.png', height: 24),
-                    label: Text('Gmail'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 48),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Sign Up',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Handle sign-up with Facebook
-                    },
-                    icon: Image.asset('assets/image/facebook.png', height: 24),
-                    label: Text('Facebook'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 48),
+                    SizedBox(height: 16),
+                    Container(
+                      constraints: BoxConstraints(
+                        maxWidth: 350,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Full Name'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your full name';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              fullName = value!;
+                            },
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              email = value!;
+                            },
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            initialValue:
+                                '', // Set the initial value to the desired phone number
+                            decoration:
+                                InputDecoration(labelText: 'Phone Number'),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your phone number';
+                              } else if (value == String) {
+                                return 'Please use phone number only';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              phoneNumber = value! as int;
+                            },
+                          ),
+                          SizedBox(height: 8),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              password = value!;
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Image.asset('assets/image/twitter.png', height: 24),
-                    label: Text('Twitter'),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(double.infinity, 48),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => VerificationCodePage()),
+                          );
+                        }
+                      },
+                      child: Text('Sign Up'),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Already have an account?'),
-                  SizedBox(width: 8),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
-                    child: Text('Sign In'),
-                  ),
-                ],
+                    SizedBox(height: 16),
+                    Text('or sign up with'),
+                    SizedBox(height: 8),
+                    Container(
+                      constraints: BoxConstraints(
+                        maxWidth: 350,
+                      ),
+                      child: Column(
+                        children: [
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              // Handle sign-up with Gmail
+                            },
+                            icon: Image.asset('assets/image/gmail.png',
+                                height: 24),
+                            label: Text('Gmail'),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 48),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          ElevatedButton.icon(
+                            onPressed: () {
+                              // Handle sign-up with Facebook
+                            },
+                            icon: Image.asset('assets/image/facebook.png',
+                                height: 24),
+                            label: Text('Facebook'),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 48),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          ElevatedButton.icon(
+                            onPressed: () {},
+                            icon: Image.asset('assets/image/twitter.png',
+                                height: 24),
+                            label: Text('Twitter'),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(double.infinity, 48),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Already have an account?'),
+                        SizedBox(width: 8),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()),
+                            );
+                          },
+                          child: Text('Sign In'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
